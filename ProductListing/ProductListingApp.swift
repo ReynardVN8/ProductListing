859f825952afefe1b031e293559a354602322a62
Reynard Vincent <reynard.vincent@metrocomjaddi.com>
2025-02-13 14:50:57 +0700

    Initial Commit

---
+++ ProductListing/ProductListingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductListingApp.swift
+//  ProductListing
+//
+//  Created by ReynardVN8 on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ProductListingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
